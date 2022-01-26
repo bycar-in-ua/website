ac7adb4bb6c35c329472b8bff7e1b9e42fe96764
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2022-01-26 16:47:44 +0200

    Hotfixes

--- components/Single/Media.vue
+++ components/Single/Media.vue
@@ -11,43 +11,48 @@ export default {
 <script setup lang="ts">
 import { Gallery } from "@/components/common/Gallery";
 import { GalleryItem } from "@/components/common/Gallery";
+import "@/assets/images/homepage-bg.jpg";
+import "@/assets/images/test-image-1.jpg";
+import "@/assets/images/test-image-2.jpg";
+import "@/assets/images/test-image-3.jpg";
+import "@/assets/images/test-image-4.jpg";
 
 const galleryItems: GalleryItem[] = [
   {
     id: 1,
     variant: "image",
-    source: "/assets/images/homepage-bg.jpg",
+    source: "@/assets/images/homepage-bg.jpg",
     alt: "Test",
   },
   {
     id: 2,
     variant: "image",
-    source: "/assets/images/test-image-1.jpg",
+    source: "@/assets/images/test-image-1.jpg",
   },
   {
     id: 3,
     variant: "image",
-    source: "/assets/images/test-image-2.jpg",
+    source: "@/assets/images/test-image-2.jpg",
   },
   {
     id: 4,
     variant: "image",
-    source: "/assets/images/test-image-3.jpg",
+    source: "@/assets/images/test-image-3.jpg",
   },
   {
     id: 5,
     variant: "image",
-    source: "/assets/images/test-image-4.jpg",
+    source: "@/assets/images/test-image-4.jpg",
   },
   {
     id: 6,
     variant: "image",
-    source: "/assets/images/test-image-2.jpg",
+    source: "@/assets/images/test-image-2.jpg",
   },
   {
     id: 7,
     variant: "image",
-    source: "/assets/images/test-image-3.jpg",
+    source: "@/assets/images/test-image-3.jpg",
   },
 ];
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 import { defineNuxtConfig } from "nuxt3";
+import { resolve } from "pathe";
 
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   build: {
     transpile: ["seemly", "naive-ui", "treemate", "vueuc", "vdirs"],
