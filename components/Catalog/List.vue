7bf49398142924c877da4452cb8d61ae720d994a
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2022-02-04 18:22:36 +0200

    Resonsive

--- components/Catalog/List.vue
+++ components/Catalog/List.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="px-5 py-8 flex-grow">
+  <div class="py-8 flex-grow">
     <SectionTitle title="Каталог" />
-    <div class="py-5 grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+    <div
+      class="py-5 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4"
+    >
       <CarCard v-for="i in 12" :key="i" />
     </div>
     <Pagination class="mt-5 justify-center" />
--- components/Single/GeneralInfo.vue
+++ components/Single/GeneralInfo.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="mb-5 grid grid-cols-3 gap-5">
-    <div class="col-span-2">
+  <div class="mb-5 grid md:grid-cols-3 gap-5">
+    <div class="md:col-span-2">
       <h3 class="mb-5 font-semibold">Описание</h3>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo ab
       molestias voluptatibus adipisci quos corrupti, tempora non architecto
