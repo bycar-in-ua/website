8dd2c03fc1ee5826b99754d5c2e0a10c03104873
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2022-05-31 17:51:17 +0300

    Runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,8 +15,10 @@ export default defineNuxtConfig({
   },
   css: ["@/assets/css/tailwind.css", "@/assets/css/global.css"],
   buildModules: ["@pinia/nuxt"],
-  publicRuntimeConfig: {
-    API_BASE: process.env.API_URL,
-    CDN_BASE: process.env.CDN_URL,
+  runtimeConfig: {
+    public: {
+      API_BASE: process.env.API_URL,
+      CDN_BASE: process.env.CDN_URL,
+    },
   },
 });
