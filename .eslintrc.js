e5c70049e770f33373a21539ce76a95e01a94524
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2022-05-31 13:10:02 +0300

    eslint installed

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "@nuxtjs/eslint-config-typescript",
+  ],
+  ignorePatterns: [".eslintrc.js"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
+  // overrides: [
+  //   {
+  //     files: [
+  //       "**/__tests__/*.{j,t}s?(x)",
+  //       "**/tests/unit/**/*.spec.{j,t}s?(x)",
+  //     ],
+  //     env: {
+  //       jest: true,
+  //     },
+  //   },
+  // ],
+};
