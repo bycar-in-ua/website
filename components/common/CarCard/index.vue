3d8c4b3dae8fd57d2edc61e4379942c11e76b744
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2022-06-17 21:38:48 +0300

    Car card reworked

--- components/common/CarCard/index.vue
+++ components/common/CarCard/index.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="card">
+  <div class="car-card">
     <NuxtLink v-if="car.featureImage" :to="carRoute">
       <img
         :src="$cdnLink(car.featureImage.path, 480, 300)"
         :alt="car.displayName"
-        class="rounded-lg mb-3"
+        class="rounded-t-lg mb-3"
       />
     </NuxtLink>
     <NuxtLink :to="carRoute">
-      <h3 class="font-bold uppercase mb-2" v-text="car.displayName" />
+      <h3
+        class="car-card-component font-bold uppercase"
+        v-text="car.displayName"
+      />
     </NuxtLink>
-    <div class="flex">
+    <div class="car-card-component flex justify-end">
       <NuxtLink :to="carRoute" class="text-primary font-bold text-sm">
-        Подробнее
+        {{ $t("details") }}
       </NuxtLink>
     </div>
   </div>
@@ -33,3 +36,16 @@ export default defineComponent({
   },
 });
 </script>
+
+<script setup lang="ts">
+const { $t } = useNuxtApp();
+</script>
+
+<style lang="postcss">
+.car-card {
+  @apply bg-white rounded-lg border border-gray-200;
+}
+.car-card-component {
+  @apply px-4 md:px-5 my-4;
+}
+</style>
