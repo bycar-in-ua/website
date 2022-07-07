f32e3dd67566200c5a7b7a95da0890ed05622db0
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2022-07-07 17:29:45 +0300

    Checkbox

--- components/Catalog/Sidebar.vue
+++ components/Catalog/Sidebar.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="bg-white p-4 my-4 rounded border border-gray-200">
     <h3 class="text-center pb-2">{{ $t("filters") }}</h3>
+
+    <Checkbox :value="true" name="test" />
   </div>
 </template>
 
@@ -10,4 +12,6 @@ export default {
 };
 </script>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Checkbox from "@/components/common/Checkbox.vue";
+</script>
