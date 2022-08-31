dd5d3ef1a677fceb0716601887d5446408429ead
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2022-08-31 12:07:06 +0300

    Minor fixes

--- components/Catalog/Sidebar.vue
+++ components/Catalog/Sidebar.vue
@@ -4,8 +4,8 @@
     :class="isSidebarShowing ? 'translate-x-0' : '-translate-x-full'"
   >
     <AdjustmentsHorizontalIcon
-      class="w-12 h-12 cursor-pointer p-2 absolute -right-12 top-14 bg-white rounded-r-lg transition-opacity lg:hidden"
-      :class="isSidebarShowing ? '' : 'opacity-70'"
+      class="w-12 h-12 cursor-pointer p-2 absolute -right-12 top-16 bg-white rounded-r-lg transition-opacity lg:hidden"
+      :class="isSidebarShowing ? '' : 'opacity-80'"
       @click="isSidebarShowing = !isSidebarShowing"
     />
     <XMarkIcon
