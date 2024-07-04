92b845ce969e6b69c7186a79d024fd90466e419c
Vladyslav Bezsmertnyi <besmercj123@gmail.com>
2024-07-04 12:35:39 +0300

    Fixing home image

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -17,7 +17,7 @@ export default defineComponent({
 <style>
 .hero {
   background: linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url("assets/images/homepage-bg.jpg");
+    url("/images/homepage-bg.jpg");
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
