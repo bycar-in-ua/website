33b6428dc366a8ea201f48a4bfa8d8d52c6928b0
Vladyslav Bezsmertnyi <56043406+Besmerch123@users.noreply.github.com>
2024-11-08 09:34:04 +0200

    BCR-21 Vehicles ordering (#2)
    * Ordering added

---
+++ components/Catalog/Headline.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useCatalogStore } from "@/stores/catalog.js";
+import type { VehiclesOrder } from "@bycar-in-ua/sdk";
+
+const options: Array<{ label: string; value: VehiclesOrder }> = [
+  { label: "За ціною: від дешевших", value: "price-asc" },
+  { label: "За ціною: від дорожчих", value: "price-desc" },
+  { label: "За роком: від новіших", value: "yearFrom-desc" },
+  { label: "За роком: від старших", value: "yearFrom-asc" },
+];
+
+const catalogStore = useCatalogStore();
+</script>
+
+<template>
+  <div class="mb-6 flex gap-5">
+    <h2 class=" font-semibold text-3xl">
+      Каталог авто
+    </h2>
+    <div class="ml-auto">
+      <USelect
+        v-model:model-value="catalogStore.order"
+        placeholder="Сортувати"
+        variant="none"
+        :options
+        :ui="{ placeholder: 'text-primary', color: { white: { none: 'text-primary' } } }"
+      />
+    </div>
+  </div>
+</template>
