4774967bf57ce55fdf83d123465ebe32f8b6bfe4
KolyaKolyaKolyadnica <105225898+KolyaKolyaKolyadnica@users.noreply.github.com>
2024-11-18 11:03:20 +0200

    BRC-35 Remove spiner from Catalog page (#8)

--- components/Catalog/Headline.vue
+++ components/Catalog/Headline.vue
@@ -16,14 +16,9 @@ const catalogStore = useCatalogStore();
 
 <template>
   <div class="flex items-center gap-5">
-    <h2 class=" font-semibold text-3xl">
+    <h2 class="font-semibold text-3xl">
       {{ t('catalog.title') }}
     </h2>
-    <UIcon
-      v-if="catalogStore.pending"
-      name="i-svg-spinners:180-ring"
-      class="w-6 h-6 text-primary"
-    />
     <div class="ml-auto">
       <USelect
         v-model:model-value="catalogStore.order"
