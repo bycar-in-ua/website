61c2a9a39f54e8941d22fde6665288ec20d25508
borkovskihnikolaj <borkovskihnikolaj@gmail.com>
2024-11-18 15:57:22 +0200

    BRC-34. Created page

---
+++ pages/about/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { generatePageTitle } from "@/utils/seo";
+
+useHead({
+  title: generatePageTitle("Про нас"),
+  meta: [
+    {
+      name: "description",
+      content: "Про нас | bycar-in-ua - Автомобільна спільнота України",
+    },
+    {
+      name: "og:title",
+      content: generatePageTitle("Про нас"),
+    },
+    {
+      name: "og:url",
+      content: "https://bycar.in.ua/about",
+    },
+    {
+      name: "og:image",
+      content: "/public/images/homepage-bg.jpg", // TODO
+    },
+  ],
+});
+</script>
+
+<template>
+  <main class="container pt-32 pb-5 mb-auto">
+    TODO: add info
+  </main>
+</template>
