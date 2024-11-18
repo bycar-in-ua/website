7769ccd810b83f8a5181be1280044659b6485b46
borkovskihnikolaj <borkovskihnikolaj@gmail.com>
2024-11-18 10:58:03 +0200

    BRC-33 Create 404 page

---
+++ error.vue
@@ -0,0 +1,11 @@
+<template>
+  <Header />
+  <div class="h-screen flex flex-col">
+    <div class="flex flex-col items-center justify-center flex-grow gap-16">
+      <h1 class="font-semibold text-3xl">
+        Нажаль, такої сторінки не існує.
+      </h1>
+    </div>
+    <Footer />
+  </div>
+</template>
