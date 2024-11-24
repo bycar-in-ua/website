fbca9125f94f4df2c9970d6e7d69f0bc1c7dc796
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2024-11-24 14:36:32 +0200

    Responsive filters

---
+++ components/Catalog/Filters/FloatingFiltersButton.vue
@@ -0,0 +1,8 @@
+<template>
+  <UButton
+    icon="i-heroicons-funnel-solid"
+    square
+    size="xl"
+    class="fixed right-4 bottom-4"
+  />
+</template>
--- components/Catalog/Sidebar.vue
+++ components/Catalog/Sidebar.vue
@@ -1,159 +1,36 @@
 <script setup lang="ts">
-import type { VehiclesFilters } from "@bycar-in-ua/sdk";
-import type { FiltersState } from "@/stores/catalog";
-import PriceFilter from "./PriceFilter.vue";
+import Filters from "./Filters/Filters.vue";
+import FloatingFiltersButton from "./Filters/FloatingFiltersButton.vue";
 
-const { t } = useI18n();
-
-const catalogStore = useCatalogStore();
-
-const isSidebarShowing = ref(false);
-
-function checkHandler<TValue extends string | number>(
-  field: keyof FiltersState,
-  checked: boolean,
-  value: TValue,
-) {
-  const existedValue = catalogStore.filters[field] ?? [];
-
-  if (!Array.isArray(existedValue)) {
-    return;
-  }
-
-  if (checked) {
-    catalogStore.updateFilters(field, [...existedValue, value]);
-  } else {
-    catalogStore.updateFilters(
-      field,
-      existedValue.filter((item) => item !== value),
-    );
-  }
-}
-
-const filters = [
-  { label: t("price"), slot: "price" },
-  { label: t("brand"), slot: "brand" },
-  { label: t("vehicle.bodyTypes.title"), slot: "bodyType" },
-  { label: t("filters.engineType.title"), slot: "engineType" },
-  { label: t("filters.drive.title"), slot: "drive" },
-];
-
-const engineTypes: NonNullable<VehiclesFilters["engineType"]> = [
-  "gas",
-  "dt",
-  "hybrid",
-  "electric",
-];
-
-const drives = [
-  "FWD", "RWD", "AWD",
-];
+const isSidebarShowing = ref(true);
 </script>
 
 <template>
   <div
-    class="w-screen sm:w-64 flex-shrink-0 fixed top-0 bottom-0 left-0 z-40 lg:z-0 lg:static lg:h-auto transition-transform lg:translate-x-0"
-    :class="isSidebarShowing ? 'translate-x-0' : '-translate-x-full'"
+    class="overflow-y-auto max-h-full pl-1 relative shrink-0 basis-64 hidden lg:block"
   >
-    <div
-      class="bg-white cursor-pointer p-2 absolute -right-12 top-16 rounded-r-lg transition-opacity lg:hidden"
-      :class="isSidebarShowing ? '' : 'opacity-80'"
-      @click="isSidebarShowing = !isSidebarShowing"
-    >
-      <UIcon name="i-heroicons-adjustments-horizontal" class="w-8 h-8" />
-    </div>
-    <UIcon
-      name="i-heroicons-x-mark"
-      class="p-2 w-8 h-8 absolute right-2 top-2 lg:hidden cursor-pointer"
-      @click="isSidebarShowing = false"
-    />
-
-    <div class="overflow-y-auto max-h-full pl-1 relative">
-      <UAccordion
-        default-open
-        multiple
-        :items="filters"
-        :ui="{
-          container:
-            'pb-4 mb-5 last-of-type:mb-0 border-b border-gray-200 last-of-type:border-0',
-        }"
-      >
-        <template #default="{ item, open }">
-          <h4 class="flex justify-between font-medium mb-4 cursor-pointer">
-            {{ item.label }}
-
-            <UIcon
-              name="i-heroicons-chevron-right-20-solid"
-              class="w-6 h-6 transform transition-transform duration-200"
-              :class="[open ? '-rotate-90 text-primary' : 'rotate-90']"
-            />
-          </h4>
-        </template>
-
-        <template #price>
-          <PriceFilter />
-        </template>
-
-        <template #brand>
-          <div class="max-h-32 overflow-y-auto">
-            <UCheckbox
-              v-for="brand in catalogStore.dictionary.brands"
-              :key="brand.id"
-              :label="brand.displayName"
-              :value="brand.id"
-              :model-value="catalogStore.filters.brand"
-              class="mb-2"
-              @change="(checked: boolean) => {
-                checkHandler('brand', checked, brand.id);
-              }"
-            />
-          </div>
-        </template>
-
-        <template #bodyType>
-          <div class="max-h-32 overflow-y-auto">
-            <UCheckbox
-              v-for="bodyType in catalogStore.dictionary.bodyTypes"
-              :key="bodyType"
-              :label="t(`vehicle.bodyTypes.items.${bodyType}`)"
-              :value="bodyType"
-              :model-value="catalogStore.filters.bodyType"
-              class="mb-2"
-              @change="(checked: boolean) => {
-                checkHandler('bodyType', checked, bodyType);
-              }"
-            />
-          </div>
-        </template>
-
-        <template #engineType>
-          <UCheckbox
-            v-for="engineType in engineTypes"
-            :key="engineType"
-            :label="t(`filters.engineType.${engineType}`)"
-            :value="engineType"
-            :model-value="catalogStore.filters.engineType"
-            class="mb-2"
-            @change="(checked: boolean) => {
-              checkHandler('engineType', checked, engineType);
-            }"
-          />
-        </template>
-
-        <template #drive>
-          <UCheckbox
-            v-for="drive in drives"
-            :key="drive"
-            :label="t(`filters.drive.${drive}`)"
-            :value="drive"
-            :model-value="catalogStore.filters.drive"
-            class="mb-2"
-            @change="(checked: boolean) => {
-              checkHandler('drive', checked, drive);
-            }"
-          />
-        </template>
-      </UAccordion>
-    </div>
+    <Filters />
   </div>
+
+  <USlideover v-model="isSidebarShowing" class="lg:hidden">
+    <div class="flex flex-col gap-5 py-6 px-2 relative overflow-x-auto">
+      <UButton
+        icon="i-heroicons-x-mark"
+        size="xl"
+        square
+        color="primary"
+        variant="link"
+        class="ml-auto"
+        @click="isSidebarShowing = false"
+      />
+      <div class="overflow-auto px-3">
+        <Filters />
+      </div>
+    </div>
+  </USlideover>
+
+  <FloatingFiltersButton
+    class="lg:hidden z-20"
+    @click="isSidebarShowing = true"
+  />
 </template>
