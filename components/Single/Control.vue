aec42284d95f540fc2cc024d293d659e5c27aace
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2024-11-24 16:05:09 +0200

    Responsive controls

--- components/Single/Control.vue
+++ components/Single/Control.vue
@@ -3,7 +3,7 @@ defineProps<{ title: string; subtitle?: string; value: string ; active?: boolean
 </script>
 
 <template>
-  <div class="flex flex-col gap-1 p-3 rounded-lg shadow-lg cursor-pointer" :title :class="active ? 'text-white bg-primary': ''">
+  <div class="flex flex-col gap-1 p-3 rounded-lg shadow-lg cursor-pointer min-w-max" :title :class="active ? 'text-white bg-primary': ''">
     <span class="text-sm font-semibold">{{ title }}</span>
     <span v-if="subtitle" class="text-[10px] " :class="active ? 'text-slate-200': 'text-slate-600'">{{ subtitle }}</span>
     <span class="text-xs">{{ value }}</span>
