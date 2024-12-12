5d3feec90af8d6cd93f37a5ec2408f2a97dd55c3
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2024-12-12 17:12:02 +0200

    Privacy page fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
 export default <Partial<Config>>{
   theme: {
@@ -49,4 +50,5 @@ export default <Partial<Config>>{
       },
     },
   },
+  plugins: [typography],
 };
