7f68622a92f37d554b04f65f9823751d262101f5
Vladyslav Bezsmertnyi <56043406+Besmerch123@users.noreply.github.com>
2024-12-13 09:12:31 +0200

    Cookie policy (#33)
    * Cookie reworked
    
    * Description prose
    
    * Cookie banner

--- app.vue
+++ app.vue
@@ -4,18 +4,29 @@
     <Header />
     <NuxtPage />
     <Footer />
+    <CookieBanner v-if="!cookieAccepted" @accept-cookie="acceptCookieHandler" />
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
 import Header from "@/components/Header/index.vue";
 import Footer from "@/components/Footer/index.vue";
+import CookieBanner from "@/components/CookieBanner.vue";
 
 const nuxtApp = useNuxtApp();
+
+const cookieAccepted = ref(true);
+
+const acceptCookieHandler = () => {
+  cookieAccepted.value = true;
+  localStorage.setItem("cookieAccepted", "true");
+};
 
 if (import.meta.client) {
   nuxtApp.hook("page:finish", () => {
     window.scrollTo(0, 0);
   });
+
+  cookieAccepted.value = localStorage.getItem("cookieAccepted") === "true";
 }
 </script>
