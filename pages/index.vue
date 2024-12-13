aea1795b1cb23bc415c4f103de700390de54942e
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2024-12-13 20:24:17 +0200

    Adding Affix

--- pages/index.vue
+++ pages/index.vue
@@ -59,7 +59,7 @@ const { data } = await useAsyncData(
   <main class="container pt-20 md:pt-10 lg:pt-0">
     <Hero :total-cars="data.totalItems" />
     <Latest :latest-items="data.latestItems" />
-    <ContactForm page="Головна сторінка" />
+    <ContactForm page="Головна сторінка" :show-affix="false" />
     <Brands :established-brands="data.establishedBrands" />
   </main>
 </template>
