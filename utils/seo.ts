3487af8245cdd70fb736a292203fb0d582f20e1a
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2024-12-17 08:37:48 +0200

    SEO fixes

--- utils/seo.ts
+++ utils/seo.ts
@@ -1,9 +1,11 @@
 const siteDomain = "bycar.in.ua";
 
+export const defaultDescription = `${siteDomain} - Легкий шлях до нового авто`;
+
 export function generatePageTitle(title: string) {
   return `${title} | ${siteDomain}`;
 }
 
 export function generatePageDescription(description: string) {
-  return `${description} | ${siteDomain} - Легкий шлях до нового авто`;
+  return `${description} | ${defaultDescription}`;
 }
