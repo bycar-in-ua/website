fc0cf96cd78f2ec868d775696245b08f3ef10a61
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2025-03-06 19:24:09 +0200

    Canonicals added

--- utils/seo.ts
+++ utils/seo.ts
@@ -3,3 +3,7 @@ const siteDomain = "bycar.in.ua";
 export function generatePageTitle(title: string) {
   return `${title} | ${siteDomain}`;
 }
+
+export function generateCanonicalUrl(path: string) {
+  return `https://${siteDomain}${path}`;
+}
