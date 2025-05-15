c749feb51a7df737591b8056f7b4ba534d1ff91d
borkovskihnikolaj <borkovskihnikolaj@gmail.com>
2025-05-15 11:53:40 +0300

    feat: added blurred element

--- components/Header/ModalHeader.vue
+++ components/Header/ModalHeader.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import BluredEllipseBlueAndYellow from "../UI/BluredEllipseBlueAndYellow.vue";
 import Logo from "../UI/Logo.vue";
 
 const props = defineProps<{ title: string }>();
@@ -6,6 +7,8 @@ const props = defineProps<{ title: string }>();
 
 <template>
   <div class="flex flex-col items-center justify-center">
+    <BluredEllipseBlueAndYellow class="absolute top-0 -z-10" />
+
     <Logo />
     <h1 class="font-semibold text-3xl text-gray-900 mt-4 mb-2">
       {{ props.title }}
