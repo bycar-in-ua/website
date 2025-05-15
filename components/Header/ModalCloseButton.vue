2cf4d71cf03d23b4b977858520c84fcfc4a7c72f
borkovskihnikolaj <borkovskihnikolaj@gmail.com>
2025-05-15 11:42:24 +0300

    feat: added login and signup modals

---
+++ components/Header/ModalCloseButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="absolute top-4 right-4 border-0">
+    <UButton icon="i-heroicons-x-mark" variant="ghost" @click="open = false" />
+  </div>
+</template>
---
+++ components/Header/ModalHeader.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Logo from "../UI/Logo.vue";
+
+const props = defineProps<{ title: string }>();
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <Logo />
+    <h1 class="font-semibold text-3xl text-gray-900 mt-4 mb-2">
+      {{ props.title }}
+    </h1>
+    <p class="text-gray-500">Доступ до вибраних авто та найкращих цін</p>
+  </div>
+</template>
