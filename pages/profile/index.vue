f16bf66f2a7ff2a64f68613681dc12c2fb8534ce
borkovskihnikolaj <borkovskihnikolaj@gmail.com>
2025-06-02 17:22:30 +0300

    Updated by design

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,17 +1,52 @@
 <script setup lang="ts">
-import BluredEllipse from "~/components/UI/BluredEllipse.vue";
 import AccoutnSettings from "../../components/Profile/AccoutnSettings.vue";
+import type { TabsItem } from "@nuxt/ui";
+
+const authStore = useAuthStore();
+
+const items = ref<TabsItem[]>([
+  {
+    label: "Персональні дані",
+    component: AccoutnSettings,
+  },
+  {
+    label: "Пропозиції",
+    disabled: true,
+  },
+]);
 </script>
 
 <template>
   <main class="container pt-24 md:pt-32 pb-5 mb-auto relative">
-    <BluredEllipse
-      class="absolute w-[410px] h-[220px] z-0 left-0 md:left-56 top-72 md:top-60"
-    />
-    <div class="lg:flex justify-center gap-10 relative blured-ellipse-bg">
-      <div class="lg:w-3/5">
-        <AccoutnSettings />
+    <div class="space-y-6">
+      <div>
+        <div class="rounded-3xl overflow-hidden h-60">
+          <img
+            src="/images/profile-bg.png"
+            alt="Profile background"
+            class="h-full object-cover"
+          />
+        </div>
+
+        <div class="relative -top-10 z-10 px-8 space-y-2">
+          <UAvatar
+            v-if="authStore.user?.avatar"
+            :src="authStore.user.avatar"
+            class="min-w-20 h-20"
+          />
+          <UIcon v-else name="i-heroicons-user-circle" class="min-w-20 h-20" />
+
+          <div class="font-semibold text-xl sm:text-3xl text-gray-900">
+            {{ authStore.getProfileName() }}
+          </div>
+        </div>
       </div>
+
+      <UTabs variant="link" :items="items" class="w-full gap-8">
+        <template #content="{ item }">
+          <component :is="item.component" />
+        </template>
+      </UTabs>
     </div>
   </main>
 </template>
