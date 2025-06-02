acb98bb2f505b7475bb12bc545383e857672fd94
borkovskihnikolaj <borkovskihnikolaj@gmail.com>
2025-06-02 17:22:30 +0300

    Updated by design

---
+++ components/Profile/SectionContainer.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{ title: string }>();
+</script>
+
+<template>
+  <div class="space-y-8">
+    <div class="space-y-5">
+      <div class="flex justify-between">
+        <div class="text-lg lg:text-xl sm:text-2xl font-medium">
+          {{ title }}
+        </div>
+
+        <div class="flex flex-col-reverse xs:flex-row justify-end gap-3">
+          <UButton variant="outline">Скинути</UButton>
+          <UButton> Зберегти </UButton>
+        </div>
+      </div>
+
+      <USeparator />
+    </div>
+
+    <slot />
+  </div>
+</template>
