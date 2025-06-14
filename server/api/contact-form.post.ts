9be82f037345d15ee41aaec8631689a3286dfd18
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2025-06-14 08:12:56 +0300

    TG bot moved to Nuxt

--- server/api/contact-form.post.ts
+++ server/api/contact-form.post.ts
@@ -1,3 +1,5 @@
+import { useTgBot } from "~/composables/useTgBot";
+
 type ContacFormPayload = {
   name: string;
   phone: string;
@@ -6,20 +8,34 @@ type ContacFormPayload = {
 
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig(event);
+  const bot = useTgBot(config.public.tgBotToken);
+
   const { page, ...body } = await readBody<ContacFormPayload>(event);
 
   const isProduction = process.env.NODE_ENV === "production";
 
   const prefix = isProduction ? "" : "STAGING";
 
-  await $fetch("/lead", {
-    method: "POST",
-    baseURL: config.public.tgBotUrl,
-    body: {
-      ...body,
-      page: [prefix, page].join(" ").trim(),
+  await bot.api.sendMessage(
+    config.public.tgLeadsChannelId,
+    `Новий лід 📞\n\n<b>Ім'я:</b> ${body.name}\n<b>Телефон:</b> ${
+      body.phone
+    }\n<b>Сторінка:</b> ${[prefix, page]
+      .join(" ")
+      .trim()}\n\nШвидше виходь на зв'язок!`,
+    {
+      parse_mode: "HTML",
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: "Копіювати телефон",
+              copy_text: { text: body.phone },
+            },
+          ],
+        ],
+      },
     },
-  });
-
+  );
   setResponseStatus(event, 201);
 });
