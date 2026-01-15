cfbeed34f77ff092c6b1a04f3d7eef2c92a420f1
Vladyslav Bezsmertnyi <besmerch123@gmail.com>
2026-01-15 15:24:30 +0200

    Auth queries reworked

--- server/api/auth/logout.post.ts
+++
@@ -1,9 +0,0 @@
-import { clearAuthCookies } from "~~/server/utils/auth-cookies";
-
-export default defineEventHandler(async (event) => {
-  await event.context.authService.logout();
-
-  clearAuthCookies(event);
-
-  return { success: true };
-});
