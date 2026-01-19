ead8686c2359583d86ca5e72a16ef27610bce2a7
borkovskihnikolaj <borkovskihnikolaj@gmail.com>
2026-01-19 15:31:00 +0200

    BCR-133: Added body-type navigation section

---
+++ app/components/UI/BodyTypeCard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{
+  item: {
+    title: string;
+    image: string;
+    query: string;
+  };
+}>();
+</script>
+
+<template>
+  <NuxtLink
+    :key="item.title"
+    :to="{
+      path: '/catalog',
+      query: {
+        bodyType: item.query,
+      },
+    }"
+  >
+    <div class="h-[135px] flex justify-center items-center">
+      <img :src="item.image" :alt="item.title">
+    </div>
+
+    <div class="text-center text-black text-xl font-semibold">
+      {{ item.title }}
+    </div>
+  </NuxtLink>
+</template>
