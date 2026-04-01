74c75e3a2b148eeb8b8fee0d751bdb2a6461eaa3
RC <roman.chentai@gmail.com>
2026-04-01 22:09:59 +0300

    about page check

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,22 +14,25 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1 ,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    launchOptions: {
+    slowMo: 500,
+  },
   },
 
   /* Configure projects for major browsers */
@@ -71,9 +74,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'pnpm dev --host localhost --port 3000',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,27 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test'
+import { link } from 'node:fs';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:3000');
+})
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test.describe('home page', () => {
+  test('should open the home page and display the correct title', async ({ page }) => {
+    await expect(page).toHaveTitle('Легкий шлях до нового авто | bycar.in.ua')
+  })
+  test('should accept the cookies', async ({page}) => {
+    await page.getByRole('button', { name: 'Зрозуміло' }).click()
+ 
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  })
+  test('find the /about, click on it, confirm this page', async ({page}) => {
+    await page.locator('header').getByRole('link', { name: 'Про нас' }).click()
+    await expect(page).toHaveURL('about')
+    await page.getByRole('button', { name: 'Зрозуміло' }).click()
+    
+    await page.mouse.wheel(0, 1500)
+    await expect(page.getByText('Вітаємо вас на сайті ByCar!')).toBeVisible()
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+  })
+})
+  
